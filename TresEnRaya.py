a69bb18353edf740e9c1874917e3ca35efbeaa82
Anxo <anxo@emailprueba.com>
2020-05-14 21:28:33 +0200

    Basicamente todo funciona, falta arreglar algún bug y documentarlo todo bien

--- TresEnRaya.py
+++ TresEnRaya.py
@@ -1,16 +1,41 @@
 import random
 from funciones import condicionVictoria, campo, TurnoJugador, TurnoMaquina
 
-
+moneda=("cara","cruz")
+monedaFliped = random.choice(moneda)
 campo()
-count = 0
-while count != 9:
-    if condicionVictoria() == 1:
-        break
-    TurnoJugador()
-    count = count+1
-    TurnoMaquina()
-    count = count+1
+print("Hay que decidir quien empieza, tu o la maquina")
+eleccion = input("Elige cara o cruz: ")
+if eleccion == monedaFliped:
+    print("Has ganado, tu empiezas")
+    marca = input("X o O? ")
+    if marca == "X":
+        marcaMaquina = "O"
+    else:
+        marcaMaquina = "X"
+    count = 0
+    while count != 9:
+        if condicionVictoria(marca,marcaMaquina) == 1:
+            break
+        TurnoJugador(marca)
+        count = count+1
+        TurnoMaquina(marcaMaquina)
+        count = count+1
+else:
+    print("Has perdido, empieza la maquina")
+    marca = input("X o O? ")
+    if marca == "X":
+        marcaMaquina = "O"
+    else:
+        marcaMaquina = "X"
+    count = 0
+    while count != 9:
+        if condicionVictoria(marca, marcaMaquina) == 1:
+            break
+        TurnoMaquina(marcaMaquina)
+        count = count+1
+        TurnoJugador(marca)
+        count = count+1
 
 
 
--- funciones.py
+++ funciones.py
@@ -12,89 +12,115 @@ def campo():
         print()
 
 
-def TurnoJugador():
+def TurnoJugador(marca):
     primer_mov = input("Introduce una posicion: ")
     if primer_mov == "1" and tablero[0][0] != "X" and tablero[0][0] != "O":
-        tablero[0][0] = "X" 
+        tablero[0][0] = marca 
     elif primer_mov == "2" and tablero[0][2] != "X" and tablero[0][2] != "O":
-        tablero[0][2] = "X"
+        tablero[0][2] = marca
     elif primer_mov == "3" and tablero[0][4] != "X" and tablero[0][4] != "O":
-        tablero[0][4] = "X"
+        tablero[0][4] = marca
     elif primer_mov == "4" and tablero[2][0] != "X" and tablero[2][0] != "O":
-        tablero[2][0] = "X"
+        tablero[2][0] = marca
     elif primer_mov == "5" and tablero[2][2] != "X" and tablero[2][2] != "O":
-        tablero[2][2] = "X"
+        tablero[2][2] = marca
     elif primer_mov == "6" and tablero[2][4] != "X" and tablero[2][4] != "O":
-        tablero[2][4] = "X"
+        tablero[2][4] = marca
     elif primer_mov == "7" and tablero[4][0] != "X" and tablero[4][0] != "O":
-        tablero[4][0] = "X"
+        tablero[4][0] = marca
     elif primer_mov == "8" and tablero[4][2] != "X" and tablero[4][2] != "O":
-        tablero[4][2] = "X"
+        tablero[4][2] = marca
     elif primer_mov == "9" and tablero[4][4] != "X" and tablero[4][4] != "O":
-        tablero[4][4] = "X"
+        tablero[4][4] = marca
     campo()
 
-def TurnoMaquina():
+def TurnoMaquina(marcaMaquina):
     print("Turno de la maquina: ")
     num_aleatorio = ("1","2","3","5","6","7","8","9")
     segundo_mov = random.choice(num_aleatorio)
     while True:
         if segundo_mov == "1" and tablero[0][0] != "X" and tablero[0][0] != "O":
-            tablero[0][0] = "O"
+            tablero[0][0] = marcaMaquina
             break
         elif segundo_mov == "2" and tablero[0][2] != "X" and tablero[0][2] != "O":
-            tablero[0][2] = "O"
+            tablero[0][2] = marcaMaquina
             break
         elif segundo_mov == "3" and tablero[0][4] != "X" and tablero[0][4] != "O":
-            tablero[0][4] = "O"
+            tablero[0][4] = marcaMaquina
             break
         elif segundo_mov == "4" and tablero[2][0] != "O" and tablero[2][0] != "O":
-            tablero[2][0] = "X"
+            tablero[2][0] = marcaMaquina
             break
         elif segundo_mov == "5" and tablero[2][2] != "X" and tablero[2][2] != "O":
-            tablero[2][2] = "O"
+            tablero[2][2] = marcaMaquina
             break
         elif segundo_mov == "6" and tablero[2][4] != "X" and tablero[2][4] != "O":
-            tablero[2][4] = "O"
+            tablero[2][4] = marcaMaquina
             break
         elif segundo_mov == "7" and tablero[4][0] != "X" and tablero[4][0] != "O":
-            tablero[4][0] = "O"
+            tablero[4][0] = marcaMaquina
             break
         elif segundo_mov == "8" and tablero[4][2] != "X" and tablero[4][2] != "O":
-            tablero[4][2] = "O"
+            tablero[4][2] = marcaMaquina
             break
         elif segundo_mov == "9" and tablero[4][4] != "X" and tablero[4][4] != "O":
-            tablero[4][4] = "O"
+            tablero[4][4] = marcaMaquina
             break
         else:
             segundo_mov = random.choice(num_aleatorio)
     campo()
 
 
-def condicionVictoria():
-        if ((tablero[0][0] == "X" and 
-        tablero[0][2] == "X" and 
-        tablero[0][4] == "X") 
-        or (tablero[2][0] == "X" and 
-            tablero[2][2] == "X" and 
-            tablero[2][4] == "X") 
-            or (tablero[4][0] == "X" and 
-                tablero[4][2] == "X" and 
-                tablero[4][4] == "X") 
-                or (tablero[0][0] == "X" and 
-                    tablero[2][0] == "X" and 
-                    tablero[4][0] == "X")
-                    or (tablero[0][2] == "X" and
-                        tablero[2][2] == "X" and
-                        tablero[4][2] == "X")
-                        or (tablero[0][4] == "X" and
-                            tablero[2][4] == "X" and
-                            tablero[4][4] == "X")
-                            or (tablero[0][0] == "X" and 
-                                tablero[2][2] == "X" and
-                                tablero[4][4] == "X")
-                                or (tablero[0][4] == "X" and
-                                    tablero[2][2] == "X" and
-                                    tablero[4][0] == "X")):
+def condicionVictoria(marca, marcaMaquina):
+        if ((tablero[0][0] == marca and 
+        tablero[0][2] == marca and 
+        tablero[0][4] == marca) 
+        or (tablero[2][0] == marca and 
+            tablero[2][2] == marca and 
+            tablero[2][4] == marca) 
+            or (tablero[4][0] == marca and 
+                tablero[4][2] == marca and 
+                tablero[4][4] == marca) 
+                or (tablero[0][0] == marca and 
+                    tablero[2][0] == marca and 
+                    tablero[4][0] == marca)
+                    or (tablero[0][2] == marca and
+                        tablero[2][2] == marca and
+                        tablero[4][2] == marca)
+                        or (tablero[0][4] == marca and
+                            tablero[2][4] == marca and
+                            tablero[4][4] == marca)
+                            or (tablero[0][0] == marca and 
+                                tablero[2][2] == marca and
+                                tablero[4][4] == marca)
+                                or (tablero[0][4] == marca and
+                                    tablero[2][2] == marca and
+                                    tablero[4][0] == marca)):
             print("Enhorabuena, has ganado")
+            return 1
+        if ((tablero[0][0] == marcaMaquina and 
+        tablero[0][2] == marcaMaquina and 
+        tablero[0][4] == marcaMaquina) 
+        or (tablero[2][0] == marcaMaquina and 
+            tablero[2][2] == marcaMaquina and 
+            tablero[2][4] == marcaMaquina) 
+            or (tablero[4][0] == marcaMaquina and 
+                tablero[4][2] == marcaMaquina and 
+                tablero[4][4] == marcaMaquina) 
+                or (tablero[0][0] == marcaMaquina and 
+                    tablero[2][0] == marcaMaquina and 
+                    tablero[4][0] == marcaMaquina)
+                    or (tablero[0][2] == marcaMaquina and
+                        tablero[2][2] == marcaMaquina and
+                        tablero[4][2] == marcaMaquina)
+                        or (tablero[0][4] == marcaMaquina and
+                            tablero[2][4] == marcaMaquina and
+                            tablero[4][4] == marcaMaquina)
+                            or (tablero[0][0] == marcaMaquina and 
+                                tablero[2][2] == marcaMaquina and
+                                tablero[4][4] == marcaMaquina)
+                                or (tablero[0][4] == marcaMaquina and
+                                    tablero[2][2] == marcaMaquina and
+                                    tablero[4][0] == marcaMaquina)):
+            print("Vaya, has perdido...")
             return 1
