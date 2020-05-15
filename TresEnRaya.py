457c2abcafb0057b3b017d278b2ff287d9ce6e65
Anxo <anxo@emailprueba.com>
2020-05-15 12:50:53 +0200

    Listo

--- TresEnRaya.py
+++ TresEnRaya.py
@@ -1,24 +1,45 @@
 import random
 from funciones import condicionVictoria, campo, TurnoJugador, TurnoMaquina
+#Todas las funciones estan explicadas en el fichero de funciones
 
+#Primero se comprueba quien empieza primero con cara o cruz
+#En el caso de que se gana en cara o cruz, el jugador empieza
+#sino empieza la maquina
 moneda=("cara","cruz")
 monedaFliped = random.choice(moneda)
+
+#1
 campo()
+
 print("Hay que decidir quien empieza, tu o la maquina")
 eleccion = input("Elige cara o cruz: ")
 if eleccion == monedaFliped:
     print("Has ganado, tu empiezas")
+
+#Ahora se eligue que tipo de marca usar
+
     marca = input("X o O? ")
     if marca == "X":
         marcaMaquina = "O"
     else:
         marcaMaquina = "X"
+
+#El contador count se usar para contar todos los movimientos
+#tanto del jugador como de la maquina
+    
     count = 0
+
+#Creo un bucle que se repitira hasta que se llene el tablero, en caso de
+#que se llene y nadie ganase, seria un empate
+
     while count != 9:
+        #4
         if condicionVictoria(marca,marcaMaquina) == 1:
             break
+        #2
         TurnoJugador(marca)
         count = count+1
+        #3
         TurnoMaquina(marcaMaquina)
         count = count+1
 else:
@@ -30,10 +51,13 @@ else:
         marcaMaquina = "X"
     count = 0
     while count != 9:
+        #4
         if condicionVictoria(marca, marcaMaquina) == 1:
             break
+        #3
         TurnoMaquina(marcaMaquina)
         count = count+1
+        #2
         TurnoJugador(marca)
         count = count+1
 
--- funciones.py
+++ funciones.py
@@ -1,9 +1,14 @@
 import random
+
+#se crea una matriz que contendra el tablero de juego
+
 tablero = [["1", " | ", "2", " | ", "3"],
            ["-", "-", "-", "-", "-", "-", "-"],
            ["4", " | ", "5", " | ", "6"],
            ["-", "-", "-", "-", "-","-", "-"],
            ["7", " | ", "8", " | ", "9"]]
+
+#La funcion campo se utiliza para mostrar el tablero
 
 def campo():
     for x in range(len(tablero)):
@@ -11,7 +16,9 @@ def campo():
             print(tablero[x][y], end=" ")
         print()
 
-
+#La funcion TurnoJugador recibe la marca elegida por el jugador (X o O)
+#el funcionamiento de esta es mirar que en el lugar que se quiere colocar la marca
+#no hay otra ya puesta
 def TurnoJugador(marca):
     primer_mov = input("Introduce una posicion: ")
     if primer_mov == "1" and tablero[0][0] != "X" and tablero[0][0] != "O":
@@ -33,6 +40,10 @@ def TurnoJugador(marca):
     elif primer_mov == "9" and tablero[4][4] != "X" and tablero[4][4] != "O":
         tablero[4][4] = marca
     campo()
+
+#Para el turno de la maquina se usa una tupla que contiene los numeros de 1 al 9
+#con random.choice eligue un numero aleatoria y mira si puede colocar la marca en ese numero
+#en el caso de que no pueda se rompe el bucle y se eligue otro numero aleatorio
 
 def TurnoMaquina(marcaMaquina):
     print("Turno de la maquina: ")
@@ -70,6 +81,9 @@ def TurnoMaquina(marcaMaquina):
             segundo_mov = random.choice(num_aleatorio)
     campo()
 
+#La funcion condicionVicoria comprueba si alguna de las dos marcas
+#cumple con los requisitos de ganar en el tres en raya
+#comprobando todos los tres en raya posibles
 
 def condicionVictoria(marca, marcaMaquina):
         if ((tablero[0][0] == marca and 
